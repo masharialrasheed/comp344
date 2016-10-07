6be92ca9d3897305145441dbc9f333942753f3b9
David H <david.hicks@students.mq.edu.au>
2016-10-08 01:51:21 +1100

    Integrated command jquery into main

--- js/superadmin.js
+++ js/superadmin.js
@@ -1,15 +1,15 @@
 $(function() {
 
-  var userRolesModule = (function() {
-    var myData = { 'users':[], 'roles':[] };
-    var currentUser = '';
+  /* Module to load table of useraccessgroups */
+  var UAGModule = (function() {
+    var myData = { 'users':[], 'roles':[], currentUser:'' };
 
     /* Cache DOM elements */
-    var $table  = $('#sa-table');
-    var $editModal = $('#edit-modal');
+    var $table      = $('#sa-table');
+    var $editModal  = $('#edit-modal');
     var $modalTitle = $editModal.find('.modal-title');
     var $modalForm  = $editModal.find('.modal-body form');
-    var $modalSave  = $editModal.find('.modal-footer button:nth-child(2)');
+    var $modalSave  = $editModal.find('#modal-submit');
 
     var tableTpl = '<thead><tr><th>id</th><th>Username</th><th>Roles</th></tr></thead>'+
       '<tbody>'+
@@ -30,10 +30,10 @@ $(function() {
     function init() {
       // Open modal form on click of user row
       $table.on('click', 'tr', function() {
-        currentUser = $(this).find('.username').text();
-        $modalTitle.text('Edit Roles - '+currentUser);
+        myData.currentUser = $(this).find('.username').text();
+        $modalTitle.text('Edit Roles - '+myData.currentUser);
         // Prefill checkboxes
-        var user = $.grep(myData.users, function(u) { return u.username == currentUser })[0];
+        var user = $.grep(myData.users, function(u) { return u.username == myData.currentUser })[0];
         $modalForm.find('input').each(function() {
           var id = parseInt($(this).val());
           if ($.inArray(id, user.role_ids) !== -1) { $(this).prop('checked', true); }
@@ -42,26 +42,30 @@ $(function() {
         $editModal.modal('show');
       });
 
+      // Submit modal on submit
       $modalSave.on('click', _submitModal);
 
       // Reset modal style when hidden
       $editModal.on('hidden.bs.modal', function () {
-        bsStyleBtn($modalSave, 'primary', 'Save');
+        help.bsStyleBtn($modalSave, 'primary', 'Save');
         $modalForm.trigger('reset');
       });
 
       _update();
     }
 
+    function destroy() {
+      $table.html('');
+
+      $table.off();
+      $modalSave.off();
+      $editModal.off();
+    }
+
     /* Render module */
     function _render() {
-      // Render table
-      var tpl = Mustache.render(tableTpl, myData.users);
-      $table.html(tpl);
-
-      // Render modal
-      var tpl = Mustache.render(checkboxTpl, myData.roles);
-      $modalForm.html(tpl);
+      $table.html(Mustache.render(tableTpl, myData.users));
+      $modalForm.html(Mustache.render(checkboxTpl, myData.roles));
     }
 
     /* Update user & roles data */
@@ -74,10 +78,8 @@ $(function() {
           myData.users = d.users;
           myData.roles = d.roles;
         })
-        .fail(logAjaxError)
-        .always(function() {
-          _render();
-        });
+        .fail(help.logAjaxError)
+        .always(_render);
     }
 
     /* On submit: send form data, style button, fade out, then hide */
@@ -91,15 +93,15 @@ $(function() {
       $.ajax({
         method: 'POST',
         url: 'api/useraccessgroup.php',
-        data: JSON.stringify({'username': currentUser, 'roles': roles}),
+        data: JSON.stringify({'username': myData.currentUser, 'roles': roles}),
         dataType: 'json'
       })
         .done(function() {
-          bsStyleBtn($modalSave, 'success', 'Saved!');
+          help.bsStyleBtn($modalSave, 'success', 'Saved!');
         })
         .fail(function(jq, status, error) {
-          bsStyleBtn($modalSave, 'danger', 'Error!');
-          logAjaxError(jq, status, error);
+          help.bsStyleBtn($modalSave, 'danger', 'Error!');
+          help.logAjaxError(jq, status, error);
         })
         .always(function() {
           _update();
@@ -107,7 +109,74 @@ $(function() {
       });
     }
 
-    function bsStyleBtn(btn, style, text) {
+    /* Return public methods */
+    return {
+      init: init,
+      destroy: destroy
+    };
+  })();
+
+
+  /* Module to load table of commands */
+  var commandModule = (function() {
+    var myData = { 'commands': [] };
+
+    /* Cache DOM elements */
+    var $table  = $('#sa-table');
+
+    var tableTpl = '<thead><tr><th>id</th><th>Command</th><th>URL</th></tr></thead>'+
+      '<tbody>'+
+      '{{#.}}'+
+        '<tr><td>{{id}}</td><td>{{name}}</td><td>{{url}}</td></tr>'+
+      '{{/.}}'+
+      '{{^.}}'+
+        '<tr><td></td><td>No records found</td><td></td></tr>'+
+      '{{/.}}'+
+      '</tbody>';
+
+    /* Initialise module */
+    function init() {
+      // Open modal form on click of row
+      // $table.on('click', 'tr', function() {});
+
+      _update();
+    }
+
+    function destroy() {
+      $table.html('');
+    }
+
+    /* Render module */
+    function _render() {
+      // Render table
+      $table.html(Mustache.render(tableTpl, myData.commands));
+    }
+
+    /* Update module */
+    function _update() {
+      $.get({
+        url: 'api/command.php',
+        dataType: 'json'
+      })
+        .done(function(d) {
+          myData.commands = d;
+        })
+        .fail(help.logAjaxError)
+        .always(function() {
+          _render();
+        });
+    }
+
+    return {
+      init: init,
+      destroy: destroy
+    };
+  })();
+
+
+  var help = {
+    /* Style Bootstrap buttons */
+    bsStyleBtn: function(btn, style, text) {
       if (style === 'success') {
         btn.removeClass('btn-primary btn-danger')
            .addClass('btn-success');
@@ -119,20 +188,31 @@ $(function() {
            .addClass('btn-primary');
       }
       btn.text(text);
-    }
+    },
 
     /* Log AJAX errors */
-    function logAjaxError(jq, status, error)  {
+    logAjaxError: function(jq, status, error)  {
         console.log("An AJAX Error Occured!");
         console.log("  Text status: " + status);
         console.log("  Error thrown: " + error);
         console.log("  jqXHR: " + JSON.stringify(jq));
     }
+  };
 
-    return {
-      init: init
-    };
-  })();
 
-  userRolesModule.init();
+  $('#pill-uag').on('click', function() {
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    commandModule.destroy();
+    UAGModule.init();
+  });
+
+  $('#pill-cmd').on('click', function() {
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    UAGModule.destroy();
+    commandModule.init();
+  });
+
+  UAGModule.init();
 });
