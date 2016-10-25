c31f2353c60b0ab89df7f0fcc1d559647462b206
David H <david.hicks@students.mq.edu.au>
2016-10-26 03:06:07 +1100

    nav fix #2

--- php/nav.php
+++ php/nav.php
@@ -1,20 +1,23 @@
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<!-- Static navbar -->
+<nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-main">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+        <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="../../index.php">RBAC</a>
+      <a class="navbar-brand" href="#">RBAC</a>
     </div>
-
-    <div id="navbar-main" class="collapse navbar-collapse">
+    <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li><a class="navbar-brand" href="LoginShopper.php">Login</a></li>
-        <li><a class="navbar-brand" href="LogoutShopper.php">Logout</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li <?php if (basename($_SERVER["PHP_SELF"]) == "LoginShopper.php") {echo 'class="active"';} ?>>
+          <a href="LoginShopper.php">Login</a></li>
+        <li><a href="LogoutShopper.php">Logout</a></li>
       </ul>
     </div>
-
   </div>
 </nav>
