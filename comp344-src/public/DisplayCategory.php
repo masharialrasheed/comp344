cefa227a7e95f667d9e94da49aca1520b0536b10
David H <david.hicks@students.mq.edu.au>
2016-10-29 01:55:26 +1100

    DisplayCategory placeholder styled

--- comp344-src/public/DisplayCategory.php
+++ comp344-src/public/DisplayCategory.php
@@ -4,13 +4,132 @@
 <head>
   <title></title>
   <?php require_once '../php/styles.php'; ?>
+  <style>
+  .product {
+    padding:10px;
+    border: 1px solid #E0E0E0;
+    border-radius: 4px;
+    margin-bottom: 20px;
+  }
+  </style>
 </head>
 <body>
 <?php require '../php/nav.php'; ?>
 
 <div class="container content">
 
-  Display Categories
+  <div class="row">
+    <ol class="breadcrumb">
+      <li><a>Home</a></li>
+      <li><a>Category 1</a></li>
+      <li class="active">Current Category</li>
+    </ol>
+  </div>
+
+  <div class="row">
+
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+
+  </div>
+
+  <div class="row">
+
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+
+  </div>
+
+  <div class="row">
+
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <!-- <img src="..." alt="..."> -->
+          <div class="caption">
+            <h4>Product</h4>
+            <p>This is a great product!</p>
+            <p><a class="btn btn-default">Buy now!</a></p>
+          </div>
+        </div>
+      </div>
+
+  </div>
 
 </div>
 
