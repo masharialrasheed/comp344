02f86f9899e09665685cf33da2ab41005bd1d5a8
David H <david.hicks@students.mq.edu.au>
2016-10-29 01:16:59 +1100

    DisplayPickList.php placeholder styled

--- comp344-src/public/DisplayPickList.php
+++ comp344-src/public/DisplayPickList.php
@@ -15,10 +15,53 @@
 
 <div class="container content">
 
-  Display Pick List
+  <ul class="nav nav-tabs nav-justified">
+    <li class="active"><a>All Orders</a></li>
+    <li><a>Pending Orders</a></li>
+    <li><a>Picked Orders</a></li>
+    <li><a>Shipped Orders</a></li>
+  </ul>
+  <br>
+  <div class="table-responsive">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>Order ID</th>
+          <th>Shopper ID</th>
+          <th>Placed On</th>
+          <th>Picked?</th>
+          <th>Shipped?</th>
+          <th>Shipped On</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </table>
+  </div>
 
 </div>
 
 <?php require '../php/footer.php'; ?>
+<script>
+var orders = [
+  ['000305', '15430', '01/07/16 11:32', '✘', '✘', ''],
+  ['000304', '19844', '01/07/16 11:31', '✓', '✘', ''],
+  ['000303', '10234', '01/07/16 11:31', '✘', '✘', ''],
+  ['000302', '21075', '01/07/16 11:31', '✓', '✘', ''],
+  ['000301', '10432', '01/07/16 11:30', '✓', '✓', '01/07/16'],
+  ['000300', '10023', '01/07/16 11:30', '✓', '✓', '01/07/16']
+];
+$(function() {
+  var $tbody = $('tbody');
+  var tableHtml = '';
+  $.each(orders, function(i, val) {
+    tableHtml += '<tr><td>'+val[0]+'</td><td>'+val[1]+'</td><td>'+val[2]+'</td>'
+              +  '<td class="text-center">'+val[3]+'</td>'
+              +  '<td class="text-center">'+val[4]+'</td><td>'+val[5]+'</td><td></tr>';
+  });
+  $tbody.html(tableHtml);
+});
+</script>
 </body>
 </html>
