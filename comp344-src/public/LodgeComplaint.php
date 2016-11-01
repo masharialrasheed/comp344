073fd66f6a8e1a363db21e711c5d2aef2c7b320c
masharialrasheed <mashari.alrasheed@students.mq.edu.au>
2016-11-01 11:31:24 +0000

    Added Lodge Complaint page

--- comp344-src/public/LodgeComplaint.php
+++ comp344-src/public/LodgeComplaint.php
@@ -35,8 +35,26 @@ $orders = getOrders();
 <div class="box">
 <h1>Lodge Complaint</h1>
 		<div>
-			
-		</div>
+        <form action="" method="post">
+        
+			<label for="textfield">Order ID:</label>
+      42612713</p>
+        <p>
+          <input type="radio" name="radio" id="radio" value="radio">
+          <label for="radio">Shipment was damaged</label>
+        </p>
+        <p>
+          <input type="radio" name="radio2" id="radio2" value="radio2">
+          <label for="radio2">item was missing</label></p>
+        <p>
+          <input type="radio" name="radio3" id="radio3" value="radio3"><label for="radio2">Shipment has not arrived after 7 Days</label>
+          </p>
+        <p>
+          <input type="submit" name="submit" id="submit" value="Submit">
+		</p>
+        </form>
+        
+        </div>
 </div>			
 </div>
 </div>
