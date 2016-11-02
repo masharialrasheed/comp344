6c49fa354ca53e86f8ce6ec8e43748eaa6b0d685
masharialrasheed <mashari.alrasheed@students.mq.edu.au>
2016-11-02 11:57:27 +0000

    Modified refund page

--- comp344-src/public/Refund.php
+++ comp344-src/public/Refund.php
@@ -6,6 +6,33 @@
 
   rbacEnforce();
   
+  if (isset($_POST['order_id'])) {
+	$sp = new SecurePay('ABC0001','abc123');
+	$sp->TestMode(); // Remove this line to actually preform a transaction
+	
+	$sp->TestConnection();
+	print_r($sp->ResponseXml);
+	
+	$sp->Cc = 4444333322221111;
+	$sp->ExpiryDate = '07/17';
+	$sp->ChargeAmount = 123;
+	$sp->ChargeCurrency = 'AUD';
+	$sp->Cvv = 321;
+	$sp->OrderId = 'ORD34234';
+	if ($sp->Valid()) { // Is the above data valid?
+		$response = $sp->Process();
+		if ($response == SECUREPAY_STATUS_APPROVED) {
+			echo "Transaction was a success\n";
+		} else {
+			echo "Transaction failed with the error code: $response\n";
+			echo "XML Dump: " . print_r($sp->ResponseXml,1) . "\n";
+		}
+	} else {
+		die("Your data is invalid\n");
+	}
+
+	
+}
 
 
 ?>
@@ -30,7 +57,7 @@
 <div class="box">
 <h1>Refund</h1>
 		<div>
-        <form action="ReservePayment.php" method="post">
+        <form action="Refund.php" method="post">
         Order ID: 1
         <br>
         
